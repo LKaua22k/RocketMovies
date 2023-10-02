b88fb458a969d7e1928ab4c9b67b2056382bccf0
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-02 14:03:50 +0000

    Terminando Tabela do user :tada:

--- src/database/knex/migrations/20230929153413_CreateUsers.js
+++ src/database/knex/migrations/20230929153413_CreateUsers.js
@@ -1,15 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("users" , table => {
+    table.increments('id')
+    table.text('name').notNullable()
+    table.text('email').notNullable()
+    table.text('password').notNullable()
+    table.text('avatar')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+});
+
+
+exports.up = knex => knex.schema.dropTable("users");
