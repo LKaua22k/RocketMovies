54285702a23667b078f10f2b6a4da26ae217fb89
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-03 20:26:18 -0300

    Criando checagem de user :tada:

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,15 @@
 const knex = require("../database/knex")
+const AppError = require("../utils/App.Error")
 
 class Usercontroller{
     async create(req,res){
         const { name,email,password } = req.body
+
+        const checkUser = await knex('users').where({email}).first()
+
+        if(checkUser){
+            throw new AppError('Usuario ja existe')
+        }
 
         await knex('users').insert({name,email,password})
 
