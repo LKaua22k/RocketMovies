101c22b2027f51e65c2edb3d0f59dd1915169ffc
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-03 16:06:47 +0000

    Adicionando a função create User :tada:

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,17 @@
 const knex = require("../database/knex");
 
 class Usercontroller{
+    async create(req,res){
+        const { name,email,password } = req.body
 
+        await knex('users').insert({
+            name,
+            email,
+            password
+        })
+
+        return res.status(201).json()
+    }
 }
 
 module.exports = Usercontroller
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 
-const usersController = require('../controllers/users.contoller')
+const usersController = require('../controllers/users.controller.js')
 
 const usercontroller = new usersController()
 
 const routes = Router()
+
+routes.use('/:id', usercontroller)
 
 module.exports = routes
 
