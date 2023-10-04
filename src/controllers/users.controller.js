1e9c4e9cec7b2371f7b570088bff76182924f86a
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-04 00:29:04 +0000

    :tada:

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs")
 const knex = require("../database/knex")
 const AppError = require("../utils/App.Error")
 
@@ -7,13 +8,15 @@ class Usercontroller{
 
         const checkUser = await knex('users').where({email}).first()
 
+        const HashPassword = await hash(password, 8)
+
         if(checkUser){
             throw new AppError('Usuario ja existe')
         }
 
-        await knex('users').insert({name,email,password})
+        await knex('users').insert({name,email,password: HashPassword})
 
-        return res.status(201).json()
+        return res.status(201).json({message: "User create"})
     }
 }
 
