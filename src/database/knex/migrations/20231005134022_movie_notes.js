e2f2d3c968581bec17bdc722673908eedd5d4ab0
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-05 15:43:36 +0000

    Criando a tabela de movie_notees :tada:

---
+++ src/database/knex/migrations/20231005134022_movie_notes.js
@@ -0,0 +1,12 @@
+exports.up = knex => knex.schema.createTable('movie_notes', table => {
+    table.increments('id')
+    table.text('title').notNullable()
+    table.text('description').notNullable()
+    table.integer('rating').notNullable()
+    table.integer('user_id').references('id').inTable('users').onDelete('CASACADE')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+})
+
+
+exports.down = knex.schema.dropTable('movie_notes')
