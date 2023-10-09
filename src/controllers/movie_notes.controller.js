f427ccc3b3f4184989dab473b12a12c12386fd1e
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-09 12:41:51 +0000

    Ajuste na criação de notas :tada:

--- src/controllers/movie_notes.controller.js
+++ src/controllers/movie_notes.controller.js
@@ -2,10 +2,27 @@ const knex = require('../database/knex')
 
 class MovieNotesController {
     async create(req,res){
-        const {title , description , rating} = req.body
+        const { title , description , rating , tags} = req.body
         const user_id = req.params
 
-        const movie_notes = await knex('movie_notes').insert({title,description,rating,user_id})
+        const note_id = await knex('movie_notes').insert({
+            title,
+            description,
+            rating,
+            user_id
+        })
+
+        const TagsInsert = tags.map( name => {
+            return {
+                name,
+                note_id,
+                user_id
+            }
+        })
+
+        await knex('movie_tags').insert(TagsInsert)
+
+        
 
         return res.json({message: "Nota criada"})
     }
